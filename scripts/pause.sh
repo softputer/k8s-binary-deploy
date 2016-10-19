88b643e66ab693b8cf96d3b151d219e9b80b1b09
Jayson <gyj3023@foxmail.com>
2016-10-19 17:42:01 +0800

    add dashboard

--- scripts/pause.sh
+++ scripts/pause.sh
@@ -2,3 +2,5 @@
 
 docker pull index.tenxcloud.com/google_containers/pause:3.0
 docker tag index.tenxcloud.com/google_containers/pause:3.0  gcr.io/google_containers/pause-amd64:3.0
+docker pull index.tenxcloud.com/google_containers/kubernetes-dashboard-amd64:v1.4.0
+docker tag index.tenxcloud.com/google_containers/kubernetes-dashboard-amd64:v1.4.0 gcr.io/google_containers/kubernetes-dashboard-amd64:v1.4.0
